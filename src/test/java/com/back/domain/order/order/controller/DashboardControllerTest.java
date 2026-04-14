144b520d8e7ab13a4943edf75e4b06e40439a1bb
LeeKW <lkw9241@gmail.com>
2026-04-14 23:00:04 +0900

    work

--- src/test/java/com/back/domain/order/order/controller/DashboardControllerTest.java
+++ src/test/java/com/back/domain/order/order/controller/DashboardControllerTest.java
@@ -1,4 +1,23 @@
 package com.back.domain.order.order.controller;
 
+import com.back.domain.order.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+
 public class DashboardControllerTest {
+
+        @Autowired
+        private OrderService orderService;
+        @Autowired
+        private MockMvc mvc;
+
 }
